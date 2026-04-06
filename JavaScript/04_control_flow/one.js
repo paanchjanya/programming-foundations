7e5a631d6255bbcd6148aa2e36a9cfb42eda8a76
Praveen Desai <desaipraveen22@gmail.com>
2026-04-06 23:47:28 +0530

    Add control flow examples in one.js

---
+++ JavaScript/04_control_flow/one.js
@@ -0,0 +1,62 @@
+// Control Flow
+
+// 1) if-statement
+
+// const temperature = 41
+// if (temperature === "54") {
+//     console.log("less than 50");
+// } else {
+
+//     console.log("greater than 50");
+// }
+
+
+// comparsion operators
+// >,<,>=,<=,==,!=, ===(also checks the type)
+
+
+// console.log(2 == "2");  O/P : True
+// console.log(2 === "2"); O/P : False
+
+
+// console.log(3 !== "2");
+
+const score = 200
+// if(score > 100) {
+//     var power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+
+const balance = 600
+// if (balance > 500) console.log("Sufficient balance."),
+// //     console.log("Line within the if condition.");
+
+// if(balance < 500) {
+//     console.log("less than 500.");
+    
+// } else if(balance < 700) {
+//     console.log("less than 700.");
+    
+// } else if(balance < 900) {
+//     console.log("less than 900.");
+    
+// } else{
+//     console.log("greater than 1000.");
+    
+// }
+
+const userLoggedIn = true
+const debitCard = false
+
+if(userLoggedIn && debitCard){
+    console.log("Allow to buy course.");
+    
+}
+
+// Logical Conditions
+
+//  || - OR Condition
+// && - AND Condition
+// ! - NOT Condition
