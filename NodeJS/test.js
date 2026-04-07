6dc5d8eb18affeefbe7a10a477417ed88e7dab73
Praveen Desai <desaipraveen22@gmail.com>
2026-04-07 07:54:22 +0530

    Create test.js

---
+++ NodeJS/test.js
@@ -0,0 +1,21 @@
+const calculator = require('./calculator')
+
+
+// console.log('I Am Self Learner.');
+
+// function sayHello(){
+//     console.log("Hello from Praveen.");
+    
+// }
+// sayHello()
+
+
+calculator.addition(3,4)
+calculator.subtraction(5,2)
+calculator.division(4,2)
+calculator.multiplication(3,4)
+
+// let age = 12
+
+// console.log(global.age);
+
